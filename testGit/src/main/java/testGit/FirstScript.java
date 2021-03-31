d073d0587b57f208af0e5962a4bd668ade50180a
poojitha <Pooja.ui88@gmai.com>
2021-03-31 16:59:42 -0400

    sum program

---
+++ testGit/src/main/java/testGit/FirstScript.java
@@ -0,0 +1,16 @@
+package testGit;
+
+public class FirstScript {
+	 
+	public static void main(String[] args) {
+		
+		 int x = 10;
+		 int y = 20;
+		 int a = x + y ;
+		 
+		
+		System.out.println("sum" +a);
+		
+	}
+
+}
