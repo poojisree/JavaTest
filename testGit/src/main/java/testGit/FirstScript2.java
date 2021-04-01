0cc25d0be2e470055df2a1606f6a0c3d41d5ecda
poojitha <Pooja.ui88@gmai.com>
2021-03-31 21:18:41 -0400

    My script2 is added

---
+++ testGit/src/main/java/testGit/FirstScript2.java
@@ -0,0 +1,21 @@
+package testGit;
+
+public class FirstScript2 {
+	     static int x = 10;
+		 static int y = 20;
+		 
+	
+		static void Add() {
+			
+			int a = x + y ;
+			System.out.println("sum" +a);
+		}		
+ public static void main(String[] args) {
+				
+				Add();
+		
+		
+		
+	}
+
+}
